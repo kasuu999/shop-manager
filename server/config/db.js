88cd7c26811a988a9e9bfd3d53c51461cf84eb95
kasuu999 <shaikhskkasim251@gmail.com>
2026-08-22 12:28:59 +0530

    purchase and reports updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,14 @@
+import { urlencoded } from 'body-parser'
 import mongoose from 'mongoose'
 const connectDB=async ()=>{
     try{
         const conn=await mongoose.connect(process.env.MONGO_URI)
         console.log(`connection hogya ${conn.connection.name}`)
     }catch(error){
-        console.log(`connection tuta${error.message}`)
+        console.log(`connection tuta shyad data connection nahi hua ${error.message}`)
     }
 
 }
 export default connectDB
+
+
